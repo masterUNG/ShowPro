95d350d4371ac0f84032cd321d8fdb03c1100dd4
masterUNG <phrombutr@gmail.com>
2016-01-21 11:01:56 +0700

    Can Update New Value to mySQL Success

--- app/src/main/java/appewtc/masterung/showpro/RegisterActivity.java
+++ app/src/main/java/appewtc/masterung/showpro/RegisterActivity.java
@@ -3,9 +3,19 @@ package appewtc.masterung.showpro;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -65,7 +75,7 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 //Update to mySQL
-
+                updateToMySQL();
                 dialogInterface.dismiss();
             }
         });
@@ -78,6 +88,45 @@ public class RegisterActivity extends AppCompatActivity {
         objBuilder.show();
 
     }   // confirmRegister
+
+    private void updateToMySQL() {
+
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> objNameValuePairs = new ArrayList<NameValuePair>();
+            objNameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            objNameValuePairs.add(new BasicNameValuePair(ManagTABLE.COLUMN_User, userString));
+            objNameValuePairs.add(new BasicNameValuePair(ManagTABLE.COLUMN_Password, passwordString));
+            objNameValuePairs.add(new BasicNameValuePair(ManagTABLE.COLUMN_Name, nameString));
+            objNameValuePairs.add(new BasicNameValuePair(ManagTABLE.COLUMN_Surname, surnameString));
+            objNameValuePairs.add(new BasicNameValuePair(ManagTABLE.COLUMN_Address, addressString));
+            objNameValuePairs.add(new BasicNameValuePair(ManagTABLE.COLUMN_Email, emailString));
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/mac/php_add_data_master.php");
+            objHttpPost.setEntity(new UrlEncodedFormEntity(objNameValuePairs, "UTF-8"));
+            objHttpClient.execute(objHttpPost);
+
+            MyAlertDialog obj2MyAlertDialog = new MyAlertDialog();
+            obj2MyAlertDialog.myDialog(RegisterActivity.this, R.drawable.icon_myaccount,
+                    "อัพโหลดได้แล้ว",
+                    "ข้อมูลได้ขึ้นไปบน Sever เรียบร้อยแล้ว");
+
+            finish();
+
+        } catch (Exception e) {
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.myDialog(RegisterActivity.this,
+                    R.drawable.icon_myaccount,
+                    "ไม่สามารถอัพข้อมูลได้",
+                    "เกิดความผิดปกติ ไม่สามารถอัพข้อมูล ขึ้น Server เพราะ " + e.toString());
+        }
+
+    }   // updateToMySQL
 
     private boolean checkSpace() {
         return userString.equals("") ||
