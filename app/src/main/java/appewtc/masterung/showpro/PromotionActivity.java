a36a30d28452685e120f35e675b7df6aeaecea05
masterUNG <phrombutr@gmail.com>
2016-02-23 11:39:20 +0700

    Create Back Promotion finish

--- app/src/main/java/appewtc/masterung/showpro/PromotionActivity.java
+++ app/src/main/java/appewtc/masterung/showpro/PromotionActivity.java
@@ -2,12 +2,26 @@ package appewtc.masterung.showpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class PromotionActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView promotionListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion);
+
+        //Bind Widget
+        promotionListView = (ListView) findViewById(R.id.listView2);
+
+    }   // Main Method
+
+    public void clickBackPromotion(View view) {
+        finish();
     }
-}
+
+}   // Main Class
